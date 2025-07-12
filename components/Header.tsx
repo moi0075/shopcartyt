11dda9524c8efe90b3ad2f5d25ac6b27e690da72
teo <teoviglietti05@gmail.com>
2025-07-12 10:05:20 +0200

    clerk acount

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,8 +7,12 @@ import CartIcon from "./CartIcon";
 import FavoriteButton from "./FavoriteButton";
 import SignIn from "./SignIn";
 import MobileMenu from "./MobileMenu";
+import { currentUser } from "@clerk/nextjs/server";
+import { ClerkLoaded, SignedIn, UserButton } from "@clerk/nextjs";
 
-const Header = () => {
+const Header = async() => {
+  const user = await currentUser()
+  console.log("Current User:", user);
   return (
     <header className="bg-white py-5 border-b border-b-black/20">
       <Container className="flex items-center justify-between text-lightColor">
@@ -22,7 +26,13 @@ const Header = () => {
           <ShearchBar />
           <CartIcon />
           <FavoriteButton />
-          <SignIn />
+          {/* <SignIn /> */}
+          <ClerkLoaded>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {!user && <SignIn />}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>
--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -5,6 +5,7 @@ import { headerData } from "@/constants/data";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import SocialMedia from "./SocialMedia";
+import { useOutsideClick } from "@/hooks";
 interface SidebarProps {
   isOpen: boolean;
   onClose: () => void;
@@ -12,12 +13,14 @@ interface SidebarProps {
 
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
   const pathname = usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose)
   return (
     <div
       className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/70 
   shadow-xl ${isOpen ? "translate-x-0" : "-translate-x-full"} hoverEffect`}
     >
       <div
+        ref = {sidebarRef}
         className="min-w-72 max-w-96 bg-black h-screen
       p-10 border-r border-r-shop_light_green flex flex-col gap-6"
       >
